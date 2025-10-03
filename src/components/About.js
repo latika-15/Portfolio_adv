59d903489366b0290ea78e0fe7c58e4f9a026905
latika-15 <lavitewari15@gamil.com>
2025-10-04 00:36:43 +0530

    about component created

---
+++ src/components/About.js
@@ -0,0 +1,55 @@
+export default function About(){
+    return(
+        <section className="section about" id="about">
+        <h2 className="title">
+            About Me
+        </h2>
+        {/* <p className="ab-srt">A Frontend Developer and Graphic Designer who loves turning ideas into interactive, eye-catching experiences. My work combines code and creativity to deliver designs that both look good and works seamlessly.</p> */}
+<div>
+        <p>
+I’m currently a pre-final year BTech student in Computer Science and Information Technology (CSIT), and my journey in the world of coding and design has been both exciting and rewarding.</p>
+
+<h3>My Journey:</h3>
+<p>
+I always loved drawing as my hobby. My curiosity about how websites and apps were built led me to explore HTML, CSS, and graphic designing tools like Canva and IbisPaint X.
+Through this my interest also started developing in UI/UX designing so I also learned how Figma works.
+</p>
+
+<h3>Foundational Learning:</h3>
+<p>
+During my first year of BTech, I focused on building a strong foundation in programming, learning languages such as C, C++ and Python. At the same time, I honed my design skills by creating digital planners. I also learned about Notion which has helped me in many ways throughout planning my learning.
+</p>
+{/* <h3>Projects & Hands-on Experience:</h3>
+<p>
+By my second year, I started integrating coding with design. I built projects like:
+
+<li>Hello Kitty-themed planner desktop app using HTML, CSS, and Electron.js.</li>
+
+<li>Solar system simulation using HTML and CSS animations.</li>
+
+<li>Tic-Tac-Toe game while learning JS.</li>
+
+<br>
+</br>
+I
+</p> */}
+
+<h3>Exploring Web Development:</h3>
+<p>
+I gradually dove into JavaScript, React, and full-stack web development. 
+<br></br>
+Now, in my 4th year, I’m sharpening my skills in UI/UX design, frontend development and AI/ML basics. I aim to create intuitive and aesthetically pleasing digital experiences.
+        </p>
+        </div>
+     
+        <h2>Contact:</h2>
+        <p>Looking forward to working with a collaborative team where my frontend and graphic design skills can help create seamless digital experiences and truly make an impact.
+            <br></br>
+            Let's connect!
+        </p>
+        <p>Email: lavit@gmail.com</p>
+
+        </section>
+        
+    )
+}
---
+++ src/components/imageMap.js
@@ -0,0 +1,7 @@
+// imageMap.js
+export const projectImages = {
+  techvision: new URL("../assets/techvision.png", import.meta.url).href,
+  dashboard: new URL("../assets/dashboard.png", import.meta.url).href,
+  solar: new URL("../assets/solar1.png", import.meta.url).href,
+  tictac: new URL("../assets/tic.png", import.meta.url).href
+};

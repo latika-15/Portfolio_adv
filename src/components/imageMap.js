c3770a3687df6c881bf551c8950a32d54a7ab962
latika-15 <166429608+latika-15@users.noreply.github.com>
2026-01-08 00:50:38 +0530

    Update imageMap.js

--- src/components/imageMap.js
+++ src/components/imageMap.js
@@ -3,5 +3,9 @@ export const projectImages = {
   techvision: new URL("../assets/techvision.png", import.meta.url).href,
   dashboard: new URL("../assets/dashboard.png", import.meta.url).href,
   solar: new URL("../assets/solar1.png", import.meta.url).href,
-  tictac: new URL("../assets/tic.png", import.meta.url).href
+  tictac: new URL("../assets/tic.png", import.meta.url).href,
+
+  // 🔹 NEW PROJECT IMAGES
+  customer_analysis: new URL("../assets/customer_analysis.png", import.meta.url).href,
+  healthtalk: new URL("../assets/healthtalk.png", import.meta.url).href,
 };
